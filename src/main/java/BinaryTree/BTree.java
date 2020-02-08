e8638618726f92780cb9814807df776063dcd95b
FuncGuy <tim25659@gmail.com>
2020-02-08 12:08:13 +0530

    Binary tree implementation with preorder, inorder and postorder traversal.

---
+++ src/main/java/BinaryTree/BTree.java
@@ -0,0 +1,135 @@
+package BinaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BTree {
+
+    static Node root;
+
+    static class Node{
+
+        public int data;
+        public Node lchild;
+        public Node rchild;
+    }
+
+    private static void preorder(Node root) {
+        if(root == null){
+            return;
+        }
+        System.out.println(root.data);
+        preorder(root.lchild);
+        preorder(root.rchild);
+    }
+
+    private static void inOrder(Node root){
+
+        if(root == null){
+            return;
+        }
+
+        inOrder(root.lchild);
+        System.out.println(root.data);
+        inOrder(root.rchild);
+    }
+
+    private static void postOrder(Node root){
+
+        if(root == null){
+            return;
+        }
+
+        postOrder(root.lchild);
+        postOrder(root.rchild);
+        System.out.println(root.data);
+    }
+
+    public static void create(){
+
+        Node p,t;
+
+        Queue<Node> q = new LinkedList<Node>();
+
+        int x;
+
+        System.out.println("Enter root value ");
+
+        Scanner s = new Scanner(System.in);
+
+        x = s.nextInt();
+
+        root = new Node();
+
+        root.data = x;
+
+        root.lchild = null;
+
+        root.rchild = null;
+
+        q.add(root);
+
+        while (!q.isEmpty()){
+
+            p = q.poll();
+
+            System.out.println("Enter left child of " + p.data);
+
+             x= s.nextInt();
+
+             if(x != -1){
+
+                 t = new Node();
+
+                 t.data = x;
+
+                 t.lchild = null;
+
+                 t.rchild = null;
+
+                 p.lchild = t;
+
+                 q.add(t);
+             }
+
+            System.out.println("Enter right child of " + p.data);
+
+            x= s.nextInt();
+
+            if(x != -1){
+
+                t = new Node();
+
+                t.data = x;
+
+                t.rchild = null;
+
+                t.rchild = null;
+
+                p.rchild = t;
+
+                q.add(t);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        create();
+
+        System.out.println("pre order");
+
+        preorder(root);
+
+        System.out.println("in order");
+
+        inOrder(root);
+
+        System.out.println("post order");
+
+        postOrder(root);
+    }
+
+}
