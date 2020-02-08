27d6dc679dcd89321f8360a5f34a6f2e465512b2
FuncGuy <tim25659@gmail.com>
2020-02-08 16:08:38 +0530

    Right view of a tree.

---
+++ src/main/java/BinaryTree/RightView.java
@@ -0,0 +1,62 @@
+package BinaryTree;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class RightView {
+
+    static class Node{
+        int key;
+        Node left;
+        Node right;
+        public Node(int key){
+            this.key = key;
+        }
+    }
+
+    public static void rightView(Node root) {
+
+        if(root == null){
+            return;
+        }
+
+        Queue<Node> q = new ArrayDeque<>();
+
+        q.add(root);
+
+        while (!q.isEmpty()){
+            int i = 0;
+            int size = q.size();
+            Node cur;
+            while (i++ < size){
+                cur = q.poll();
+                if(i == size){
+                    System.out.println(cur.key + " ");
+                }
+                if(cur.left != null){
+                    q.add(cur.left);
+                }
+
+                if(cur.right != null){
+                    q.add(cur.right);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.right = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+        root.right.left.left = new Node(7);
+        root.right.left.right = new Node(8);
+
+        rightView(root);
+    }
+
+}
