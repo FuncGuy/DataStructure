df7dee046d252775f3a1ba894878b0af5e98b254
FuncGuy <tim25659@gmail.com>
2020-02-09 12:06:07 +0530

    Min Heap insertion.

--- src/main/java/Heap/MaxHeap.java
+++ src/main/java/Heap/MaxHeap.java
@@ -2,13 +2,34 @@ package Heap;
 
 public class MaxHeap {
 
-    public void insert(int []A ,int n) {
+    public void insertMax(int[] A ,int n) {
+
         int temp, i = n;
+
         temp = A[i];
+
         while (i > 1 && temp > A[i/2]){
+
             A[i] = A[i / 2];
+
             i = i / 2;
         }
+
+        A[i] = temp;
+    }
+
+    public void insertMin(int[] A, int n) {
+
+        int temp ,i = n;
+
+        temp = A[n];
+
+        while (i > 1 && temp < A[i/2]) {
+            A[i] = A[i/2];
+
+            i = i / 2;
+        }
+
         A[i] = temp;
     }
 }
